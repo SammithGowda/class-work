b56bd34ce58bd2f5fda1d07826a8e501ae0028ba
Sammith1 <87310964+Sammith1@users.noreply.github.com>
2022-03-03 11:54:48 +0530

    commit

---
+++ assign1/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+module.exports={
+    entry:"./src/index.js",
+    output:{
+        path:path.resolve(__dirname,"build"),
+        filename: "bundle.js"
+
+    },
+    module: {
+        rules: [{ test: /\.css$/, use: ["style-loader","css-loader"]
+     },
+     {
+        test: /\.(png|jp(e*)g|svg)$/,  
+        use: [{
+            loader: 'url-loader',
+            options: { 
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            } 
+        }]
+    }
+    ],
+      },
+
+}
