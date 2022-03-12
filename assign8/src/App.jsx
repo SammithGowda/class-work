f2d3f5d8e4649af07c6b71f6690fee1fddd30fc6
Sammith1 <87310964+Sammith1@users.noreply.github.com>
2022-03-12 16:19:01 +0530

    commit

---
+++ assign8/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import{Button} from "./componets/Button"
+import './App.css'
+
+function App() {
+  const [theme1, setTheme1] = useState("first_btn")
+ 
+return (
+  <div className="App">
+
+    <Button theme1={theme1}>Primary Button</Button> {/* bg coloer blue*/}
+    <Button  >Default Button</Button>
+    <Button theme1={"dott"} >Dashed Button </Button> <br/>
+    <Button theme1={"textbt"} >Text Button</Button>
+    <Button theme1={"lestbt"}>Link Button </Button>{/*colore blue no border*/}
+   
+  </div>
+)
+}
+
+
+export default App
